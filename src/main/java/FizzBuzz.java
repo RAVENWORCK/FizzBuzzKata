d39b326b3115a707f29be60a3b06a3dfb6bf3849
Raven Worck <www.hoomankh1379@gmail.com>
2022-04-23 23:35:35 -0700

    done

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,5 +1,24 @@
 public class FizzBuzz {
-    public int play(int adad) {
-        return adad;
+    public String play(int adad) {
+
+        if (isDevisbleBy3(adad) && isDevisblBy5(adad)){
+            return "fizz Buzz";
+        }
+        if(isDevisbleBy3(adad)){
+            return "fizz";
+        }
+        if(isDevisblBy5(adad)){
+            return "fuzz";
+        }
+
+        return String.valueOf(adad);
+    }
+
+    private boolean isDevisbleBy3(int adad) {
+        return adad % 3 == 0;
+    }
+
+    private boolean isDevisblBy5(int adad) {
+        return adad % 5 == 0;
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -9,13 +9,54 @@ public class FizzBuzzTest {
         FizzBuzz fizzBuzz = new FizzBuzz();
 
         //when
-        int result = fizzBuzz.play(1);
+        String result = fizzBuzz.play(1);
         //then
-        Assert.assertEquals(1 , result);
+        Assert.assertEquals("1" , result);
     }
 
     @Test
-    public void give_2_should_return2() {
+    public void give_2_should_return_2() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
 
+        String result = fizzBuzz.play(2);
+
+        Assert.assertEquals("2" , result);
+    }
+
+    @Test
+    public void give_3_should_return_fizz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String result = fizzBuzz.play(3);
+
+        Assert.assertEquals("fizz", result);
+    }
+
+    @Test
+    public void give_4_return_4() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String result = fizzBuzz.play(4);
+
+        Assert.assertEquals("4" , result);
+
+    }
+
+    @Test
+    public void give_5_return_fuzz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String result = fizzBuzz.play(5);
+
+        Assert.assertEquals("fuzz" , result);
+    }
+
+    @Test
+    public void give_15_return_fizz_buzz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String result = fizzBuzz.play(15);
+
+        Assert.assertEquals("fizz Buzz" , result);
     }
 }
